3bdd8faaa548cac7b01f9ae747f7a37a82bd3db1
sanjay-749 <rsanjayraj2005@gmail.com>
2025-10-06 18:01:54 +0530

    Initial commit

---
+++ ride-sharing-app/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AppRouter from "./routes/AppRouter";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <AppRouter />
+    </div>
+  );
+}
---
+++ ride-sharing-app/src/pages/user/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Login Page</h1>
+      <p>Please login to continue.</p>
+    </div>
+  );
+}
